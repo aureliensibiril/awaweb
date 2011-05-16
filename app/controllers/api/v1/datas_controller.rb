9ae4f85decb532f3d34a560399c289c9ffe29397
Aurelien Sibiril <sibiril@mac.com>
2011-05-16 17:28:50 +0300

    api change

--- app/controllers/api/v1/datas_controller.rb
+++ app/controllers/api/v1/datas_controller.rb
@@ -3,7 +3,7 @@ class Api::V1::DatasController < ApplicationController
   def index
     range = "created_at #{(1.day.ago.utc...Time.now.utc).to_s(:db)} AND bottle_id = #{params[:id]}"
     @today = Refill.all(:conditions => range)
-    @alltime = Refill.all(:bottle_id => params[:id])
+    @alltime = Refill.all(:conditions => "bottle_id = #{params[:id]}")
     
     @return = ["today" => @today.length,
                 "alltime" => @alltime.length]
