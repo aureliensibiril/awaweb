72c2f2a24b28cd39e60f7d6df27c392f92642ca7
Aurelien Sibiril <sibiril@mac.com>
2011-05-16 18:23:27 +0300

    maj dynamic du dashboard

--- app/controllers/api/v1/datas_controller.rb
+++ app/controllers/api/v1/datas_controller.rb
@@ -1,19 +1,15 @@
 class Api::V1::DatasController < ApplicationController
 
   def index
-    
-    
-      @bottle = Bottle.find_by_serial(params[:id])
-      
+
+    @bottle = Bottle.find_by_serial(params[:id])
     range = "created_at #{(1.day.ago.utc...Time.now.utc).to_s(:db)} AND bottle_id = #{@bottle.id}"
     @today = Refill.all(:conditions => range)
     @alltime = Refill.all(:conditions => "bottle_id = #{@bottle.id}")
-    
-   
-    
+
     @return = ["today" => @today.length,
-                "alltime" => @alltime.length]
-    render :json => @return
+      "alltime" => @alltime.length]
+      render :json => @return
+    end
+
   end
-  
-end
--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -8,6 +8,18 @@ class DashboardController < ApplicationController
     @validate = FALSE
     if @user.bottles.count > 0
       @validate = TRUE
+      @bottle = @user.bottles.first
+      range = "created_at #{(1.day.ago.utc...Time.now.utc).to_s(:db)} AND bottle_id = #{@bottle.id}"
+      @today = Refill.all(:conditions => range)
+      @alltime = Refill.all(:conditions => "bottle_id = #{@bottle.id}")
+      if @today.length > 0
+        @daily = ((0.8 * @today.length) / 1.5 ) * 100  
+      end
+      
+      if @alltime.length > 0
+        @money = @alltime.length * 0.3
+        @garbage = @alltime.length * 0.2 
+      end
     end
     
   end
