4ce8e70506a29a62dab0b629814969417f65da7c
Aurelien Sibiril <sibiril@mac.com>
2011-05-16 18:03:15 +0300

    api change

--- app/controllers/api/v1/datas_controller.rb
+++ app/controllers/api/v1/datas_controller.rb
@@ -1,9 +1,15 @@
 class Api::V1::DatasController < ApplicationController
 
   def index
-    range = "created_at #{(1.day.ago.utc...Time.now.utc).to_s(:db)} AND bottle_id = #{params[:id]}"
+    
+    
+      @bottle = Bottle.find_by_serial(params[:id])
+      
+    range = "created_at #{(1.day.ago.utc...Time.now.utc).to_s(:db)} AND bottle_id = #{@bottle.id}"
     @today = Refill.all(:conditions => range)
-    @alltime = Refill.all(:conditions => "bottle_id = #{params[:id]}")
+    @alltime = Refill.all(:conditions => "bottle_id = #{@bottle.id}")
+    
+   
     
     @return = ["today" => @today.length,
                 "alltime" => @alltime.length]
--- app/controllers/api/v1/refills_controller.rb
+++ app/controllers/api/v1/refills_controller.rb
@@ -3,7 +3,8 @@ class Api::V1::RefillsController < ApplicationController
   def new
     @refill = Refill.new
     @refill.genre = params[:type]
-    @refill.bottle_id = params[:id]
+    @bottle = Bottle.find_by_serial(params[:id])
+    @refill.bottle_id = @bottle.id
     @refill.save
     render :text => "ok"
   end
